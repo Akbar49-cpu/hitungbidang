cfb050e306b99db88e988578fea268b2cdc8f788
Perjalanan Ziarah <perjalananziarah@gmail.com>
2020-04-14 09:27:07 +0700

    Initial commit

---
+++ app/src/main/java/com/relative/apikasihitungbidang/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.relative.apikasihitungbidang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
